b458d521327ec788ab464847fbca5c278bd412c7
Ali Ok <aliok@redhat.com>
2023-09-08 10:41:46 +0300

    Enable 3scale plugin

--- packages/backend/src/plugins/catalog.ts
+++ packages/backend/src/plugins/catalog.ts
@@ -2,13 +2,28 @@ import { CatalogBuilder } from '@backstage/plugin-catalog-backend';
 import { ScaffolderEntitiesProcessor } from '@backstage/plugin-scaffolder-backend';
 import { Router } from 'express';
 import { PluginEnvironment } from '../types';
+import { ThreeScaleApiEntityProvider } from '@janus-idp/backstage-plugin-3scale-backend';
+
 
 export default async function createPlugin(
   env: PluginEnvironment,
 ): Promise<Router> {
   const builder = await CatalogBuilder.create(env);
+
+
+  const three = ThreeScaleApiEntityProvider.fromConfig(env.config, {
+          logger: env.logger,
+          scheduler: env.scheduler,
+      });
+
+  builder.addEntityProvider(
+      three
+  );
+
   builder.addProcessor(new ScaffolderEntitiesProcessor());
   const { processingEngine, router } = await builder.build();
   await processingEngine.start();
+
+
   return router;
 }
